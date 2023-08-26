6ab766c06d6eb6b46d7c5b4b39b99df58a620c0a
Amanda Mitchell <she@mandymitchell.me>
2023-08-26 11:28:19 -0700

    fix: Properly import chalk.

---
+++ __tests__/cli.js
@@ -0,0 +1,10 @@
+const spawn = require('cross-spawn');
+const path = require('path');
+
+test('runs without errors', () => {
+	const result = spawn.sync('node', [path.join(__dirname, '..', 'bin', 'index.js'), '-d', '.'], {
+		stdio: 'inherit',
+	});
+
+	expect(result.status).toBe(0);
+});
--- bin/index.js
+++ bin/index.js
@@ -8,67 +8,86 @@ require('please-upgrade-node')(pkg);
 const fs = require('fs');
 const { createRequire } = require('module');
 const path = require('path');
-const chalk = require('chalk');
 const spawn = require('cross-spawn');
 
 const workingDirectoryRequire = createRequire(path.resolve(process.cwd(), 'index.js'));
 
+const chalkImport = import('chalk');
+
+async function logWarning(...args) {
+	const { default: chalk } = await chalkImport;
+	console.warn(chalk.yellow(...args));
+}
+
+async function logError(...args) {
+	const { default: chalk } = await chalkImport;
+	console.error(chalk.red(...args));
+}
+
 try {
 	workingDirectoryRequire('eslint');
 } catch (x) {
-	console.error(chalk.red('eslint was not found.'));
-	console.error(
-		chalk.red('suppress-eslint-errors requires eslint to be installed in the working directory.')
-	);
-	process.exit(1);
+	Promise.all([
+		logError('eslint was not found.'),
+		logError('suppress-eslint-errors requires eslint to be installed in the working directory.'),
+	]).finally(() => process.exit(1));
 }
 
 const jscodeshiftPath = require.resolve('jscodeshift/bin/jscodeshift');
 const transformPath = require.resolve('../transforms/suppress-eslint-errors');
 
-const gitignoreArguments = [];
-const gitignorePath = path.resolve(process.cwd(), '.gitignore');
-if (fs.existsSync(gitignorePath)) {
-	if (
-		fs
-			.readFileSync(gitignorePath, { encoding: 'utf8' })
-			.split('\n')
-			.findIndex((line) => line.startsWith('!')) !== -1
-	) {
-		console.warn(
-			chalk.yellow(
-				'your .gitignore contains exclusions, which jscodeshift does not properly support.'
-			)
-		);
-		console.warn(chalk.yellow('skipping the ignore-config option.'));
-	} else {
-		gitignoreArguments.push(`--ignore-config=.gitignore`);
+async function findGitignoreArguments() {
+	const gitignorePath = path.resolve(process.cwd(), '.gitignore');
+
+	if (!fs.existsSync(gitignorePath)) {
+		return [];
 	}
+
+	const allLines = fs.readFileSync(gitignorePath, { encoding: 'utf8' }).split('\n');
+	if (allLines.findIndex((line) => line.startsWith('!')) !== -1) {
+		await logWarning(
+			'your .gitignore contains exclusions, which jscodeshift does not properly support.'
+		);
+		await logWarning('skipping the ignore-config option.');
+
+		return [];
+	}
+
+	return [`--ignore-config=.gitignore`];
 }
 
-const result = spawn.sync(
-	'node',
-	[jscodeshiftPath, '--no-babel', '-t', transformPath]
-		.concat(gitignoreArguments)
-		.concat(process.argv.slice(2)),
-	{
-		stdio: 'inherit',
-	}
-);
+(async function runJsCodeShift() {
+	const result = spawn.sync(
+		'node',
+		[
+			jscodeshiftPath,
+			'--no-babel',
+			'-t',
+			transformPath,
+			...(await findGitignoreArguments()),
+			...process.argv.slice(2),
+		],
+		{
+			stdio: 'inherit',
+		}
+	);
 
-if (result.signal) {
-	if (result.signal === 'SIGKILL') {
-		console.error(
-			'The script failed because the process exited too early. ' +
-				'This probably means the system ran out of memory or someone called ' +
-				'`kill -9` on the process.'
-		);
-	} else if (result.signal === 'SIGTERM') {
-		console.error(
-			'The script failed because the process exited too early. ' +
-				'Someone might have called `kill` or `killall`, or the system could ' +
-				'be shutting down.'
-		);
+	if (result.signal) {
+		if (result.signal === 'SIGKILL') {
+			console.error(
+				'The script failed because the process exited too early. ' +
+					'This probably means the system ran out of memory or someone called ' +
+					'`kill -9` on the process.'
+			);
+		} else if (result.signal === 'SIGTERM') {
+			console.error(
+				'The script failed because the process exited too early. ' +
+					'Someone might have called `kill` or `killall`, or the system could ' +
+					'be shutting down.'
+			);
+		}
+		process.exit(1);
 	}
-	process.exit(1);
-}
+
+	process.exit(result.status);
+})();
